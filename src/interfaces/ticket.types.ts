8f2e12bf6c8e0c07ebfdfc9d271a0d73972d5711
sabbar@dev <sabbar.work@gmail.com>
2023-09-25 18:12:32 -0400

    initial

--- src/interfaces/ticket.types.ts
+++
@@ -1,6 +0,0 @@
-export interface FormData {
-  name: string;
-  email: string;
-  description: string;
-}
-export type PartialFormData = Partial<FormData>;
--- src/services/createTask.service.ts
+++
@@ -1,6 +0,0 @@
-import axios from "axios";
-import { FormData } from "../interfaces/ticket.types";
-
-export const createTicket = async (url: string, data: FormData) => {
-  return await axios.post(url, data);
-};
